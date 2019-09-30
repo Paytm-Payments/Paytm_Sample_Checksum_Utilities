b7066b9dbb8e00125551a762f72069c70b6fd3bb
Sunny  pranay <mpranay2017@gmail.com>
2019-10-01 01:43:05 +0530

    In encode function added utf-8 encoding.
    In Python 3, we have to encode it into a bytearray.

--- Python/Python v3.x/Checksum.py
+++ Python/Python v3.x/Checksum.py
@@ -83,8 +83,9 @@ def __encode__(to_encode, iv, key):
     # Pad
     to_encode = __pad__(to_encode)
     # Encrypt
-    c = AES.new(key, AES.MODE_CBC, iv)
-    to_encode = c.encrypt(to_encode)
+    c = AES.new(key.encode("utf8"), AES.MODE_CBC, iv.encode("utf8"))
+    #key_as_bytearray.decode("utf8")
+    to_encode = c.encrypt(to_encode.encode("utf8"))
     # Encode
     to_encode = base64.b64encode(to_encode)
     return to_encode.decode("UTF-8")
